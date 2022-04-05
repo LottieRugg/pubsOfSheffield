e1f1ffe218780c2fd9f4e040fc556df4a8f10a36
Lottie Rugg <lottie.ruggeasey@gmail.com>
2022-04-05 14:02:09 +0100

    Form designed in html

--- pubs_of_sheffield.php
+++ pubs_of_sheffield.php
@@ -2,6 +2,7 @@
 require 'functions.php';
 $db = connectToDatabase();
 $itemsList = getAllItems($db);
+session_start()
 ?>
 
 
@@ -9,10 +10,6 @@ $itemsList = getAllItems($db);
 <html>
 <head>
     <link rel="stylesheet" href="pubs_stylesheet.css" type="text/css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Poppins:ital,wght@0,100;0,200;0,300;1,100;1,200;1,400&display=swap"
-          rel="stylesheet">
 </head>
 <body>
 
@@ -24,7 +21,45 @@ $itemsList = getAllItems($db);
     echo selectItem($itemsList);
     ?>
 
-</div>
 
+    <div class="collection_item">
+        <h3> Add a pub!</h3>
+        <h5>Use this form to add new pubs to the collection!</h5>
+        <form method="post" class="input_form">
+            <label>Pub name:
+                <input type="text" name="name" required class="input_item">
+            </label>
+            <label>What is the pub's address?
+                <input type="text" name="address" class="input_item">
+            </label>
+            <label> What is rating out of ten?
+                <input type="number" min="0" max="10" name="rating" class="input_item">
+            </label>
+            <label>What is their local brewery?
+                <input type="text" name="local_brewery" class="input_item">
+            </label>
+            <label>Include a link to a picture here:
+                <input type="url" name="picture" class="input_item">
+            </label>
+            <label>What should I order if I get to the bar first?
+                <input type="text" name="what_to_order" class="input_item">
+            </label>
+            <label class="open_fire">
+                <div class="open_fire_label">
+                    Do they have an open fire?
+                </div>
+                <label class="radio_button">yes
+                    <input type="radio" id="yes" name="open_fire" value="1">
+                </label>
+                <label class="radio_button">no
+                    <input type="radio" id="no" name="open_fire" value="0">
+                </label>
+            </label>
+            <div class="submit">
+                <input type="submit" class="submit_button" value="Collect your pub!">
+            </div>
+        </form>
+    </div>
+</div>
 </body>
 </html>
